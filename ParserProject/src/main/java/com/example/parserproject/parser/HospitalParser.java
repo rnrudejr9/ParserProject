5cb8bbc3c7cddab1ca5e558c9f17af6265510a36
Koogling <rnrudejr1234@naver.com>
2022-10-31 14:34:37 +0900

    생성자 Test 통과 + csv파일 추가

--- ParserProject/src/main/java/com/example/parserproject/parser/HospitalParser.java
+++ ParserProject/src/main/java/com/example/parserproject/parser/HospitalParser.java
@@ -3,31 +3,36 @@ package com.example.parserproject.parser;
 import com.example.parserproject.domain.Hospital;
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Date;
 
 public class HospitalParser implements Parser<Hospital> {
     @Override
     public Hospital parse(String str) {
 
-        String[] row =str.replace("\"","").split(",");
-        System.out.println(Arrays.toString(row));
+        String[] row =str.split("\",\"");
         Hospital hospital = new Hospital();
-        hospital.setId(Integer.parseInt(row[0]));
+        hospital.setId(Integer.parseInt(row[0].replace("\"","")));
         hospital.setOpenServiceName(row[1]);
-        hospital.setOpenLocalGovernmentCode(Integer.parseInt(row[2]));
-        hospital.setManagementNumber(row[3]);
-        hospital.setLicenseDate(row[4]);
-        hospital.setBusinessStatus(Integer.parseInt(row[5]));
-        hospital.setBusinessStatusCode(Integer.parseInt(row[6]));
-        hospital.setPhone(row[7]);
-        hospital.setFullAddress(row[8]);
-        hospital.setRoadNameAddress(row[9]);
-        hospital.setHospitalName(row[10]);
-        hospital.setBusinessTypeName(row[11]);
-        hospital.setHealthcareProviderCount(Integer.parseInt(row[12]));
-        hospital.setPatientRoomCount(Integer.parseInt(row[13]));
-        hospital.setTotalNumberOfBeds(Integer.parseInt(row[14]));
-        hospital.setTotalAreaSize(Integer.parseInt(row[15]));
+        hospital.setOpenLocalGovernmentCode(Integer.parseInt(row[3]));
+        hospital.setManagementNumber(row[4]);
+        int year = Integer.parseInt(row[5].substring(0,4));
+        int month = Integer.parseInt(row[5].substring(4,6));
+        int day = Integer.parseInt(row[5].substring(6,8));
+        hospital.setLicenseDate(LocalDateTime.of(year,month,day,0,0,0));
+        hospital.setBusinessStatus(Integer.parseInt(row[7]));
+        hospital.setBusinessStatusCode(Integer.parseInt(row[9]));
+        hospital.setPhone(row[15]);
+        hospital.setFullAddress(row[18]);
+        hospital.setRoadNameAddress(row[19]);
+        hospital.setHospitalName(row[21]);
+        hospital.setBusinessTypeName(row[25]);
+        hospital.setHealthcareProviderCount(Integer.parseInt(row[29]));
+        hospital.setPatientRoomCount(Integer.parseInt(row[30]));
+        hospital.setTotalNumberOfBeds(Integer.parseInt(row[31]));
+        hospital.setTotalAreaSize(Float.parseFloat(row[32]));
+
         return new Hospital();
 
     }
