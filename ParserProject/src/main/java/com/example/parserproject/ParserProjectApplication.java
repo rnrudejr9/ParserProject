53e30ecc8146bd407cd9b56970f3a360d82ff7db
Koogling <rnrudejr1234@naver.com>
2022-10-31 11:19:39 +0900

    first commit

---
+++ ParserProject/src/main/java/com/example/parserproject/ParserProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.parserproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParserProjectApplication.class, args);
+    }
+
+}
