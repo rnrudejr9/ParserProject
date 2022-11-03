7fe17f70c37579bfd346c60b86259872c70bd7aa
Koogling <rnrudejr1234@naver.com>
2022-11-03 11:37:48 +0900

    deleteall api

--- src/main/java/com/example/parserproject/controller/HospitalController.java
+++ src/main/java/com/example/parserproject/controller/HospitalController.java
@@ -5,10 +5,7 @@ import com.example.parserproject.domain.Hospital;
 import com.example.parserproject.domain.dao.HospitalDao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,21 +17,21 @@ public class HospitalController {
     public HospitalController(HospitalDao hospitalDao) {
         this.hospitalDao = hospitalDao;
     }
-
     @GetMapping("/getall")
     public List<Hospital> getHospital(){
         return hospitalDao.findAll();
     }
-
     @GetMapping("/get/{id}")
     public String get(@PathVariable Integer id) {
         Hospital hospital = hospitalDao.findById(id);
         return hospital.getHospitalName();
     }
-
-
     @GetMapping("/isfail")
     public List<Hospital> isFailed(){
         return hospitalDao.findByFail();
+    }
+    @DeleteMapping("/delete")
+    public void deleteAll(){
+        hospitalDao.deleteAll();
     }
 }
